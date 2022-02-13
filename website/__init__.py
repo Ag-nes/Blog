2af2223e8a427e46c535227930711b5d97f51ac5
Agnes Okutoyi <agnes.okutoyi@student.moringaschool.com>
2022-02-13 17:02:59 +0300

    add prefix

--- website/__init__.py
+++ website/__init__.py
@@ -8,4 +8,10 @@ def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = "helloworld"
 
+    from .views import views
+
+    app.register_blueprint(views, url_prefix="/")
+
+    return app
+
    
