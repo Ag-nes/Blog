2b82e35a0ff17a08e1dc0307bfdf6ab6b6e6c075
Agnes Okutoyi <agnes.okutoyi@student.moringaschool.com>
2022-02-16 00:50:39 +0300

    deployment

--- manage.py
+++ manage.py
@@ -4,7 +4,7 @@ from flask_script import Manager, Server
 from flask_migrate import Migrate, MigrateCommand
 
 
-app = create_app('dev')
+app = create_app('production')
 
 manager = Manager(app)
 manager.add_command('server', Server)
