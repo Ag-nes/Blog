555bb06a63ea005bca91c569c00a1bf539da66cf
Agnes Okutoyi <agnes.okutoyi@student.moringaschool.com>
2022-02-16 22:02:48 +0300

    edit

--- config.py
+++ config.py
@@ -8,11 +8,11 @@ class Config:
     UPLOADED_PHOTOS_DEST='app/static/photos'
 
     #email config
-    MAIL_SERVER = 'smpt.googlemail.com'
+    MAIL_SERVER = 'faithagnes2@gmail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
-    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    MAIL_USERNAME = os.environ.get("faithagnes2@gmail.com")
+    MAIL_PASSWORD = os.environ.get("@liammyfirst1")
 
 class ProductionConfig(Config):
     '''
